45ae29488e5d7af02a5c347df14bde6528e0f9e9
motadaniela <40210288@esmad.ipp.pt>
2022-01-08 17:19:50 +0000

    commit

--- AED/trabalho.py
+++ AED/trabalho.py
@@ -2,36 +2,55 @@
 #Daniela Moreira, nº aluno: 40210349
 #Daniela Monteiro, nº aluno: 40210288
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter.ttk import Combobox
+from PIL import ImageTk,Image
+import os
 
 window = Tk()
-window.geometry("1000x700")
+scrn_width = window.winfo_screenwidth()
+scrn_height = window.winfo_screenheight()
+app_w = 1000
+app_h = 600
+x=(scrn_width/2)-(app_w/2)
+y=(scrn_height/2)-(app_h/2)
+window.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}".format(app_w,app_h,int(x),int(y)))
 window.title('Gestor de filmes e séries')
+'''????
+lista_filmes =[]
+lista_categorias=[]'''
 
-#login
-data = "userdata.txt"
+def catalogo():
+    n=1
+def favoritos():
+    wFavoritos = Tk()
+    wFavoritos.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}".format(app_w,app_h,int(x),int(y)))
+    wFavoritos.title('Gestor de filmes e séries')
+    wFavoritos.focus_force()
+    wFavoritos.grab_set()
+    pannel1 = PanedWindow()
 
-login = Button(window, text="Iniciar sessão", fg='blue', width = 20)
-login.place(x=10, y=10)
+def log_in():
+    n=1
+def sair():
+    res = messagebox.askquestion("Sair","Deseja sair?")
+    if res=="yes":
+        window.destroy()
 
-#sidebar
-sidebar = Menu(window)
-#filmes
-filmes = Menu(sidebar)
-filmes.add_command(label="Nome de filme", command="noaction")
-sidebar.add_cascade(label="Filmes", menu = sidebar)
-#series
-series = Menu(sidebar)
-series.add_command(label="Nome de série", command="noaction")
-sidebar.add_cascade(label="Séries", menu = sidebar)
-#categorias
-categories = Menu(sidebar)
-categories.add_command(label="categoria", command="noaction")
-sidebar.add_cascade(label="Categorias", menu = sidebar)
-#Top rated
-toprated = Menu(sidebar)
-toprated.add_command(label="Nome", command="noaction")
-sidebar.add_cascade(label="Top rated", menu = sidebar)
+#menu
+main_menu = Menu(window)
+main_menu.add_command(label="Catálago", command=catalogo)
+main_menu.add_command(label="Favoritos", command=favoritos)
+main_menu.add_command(label="Login", command=log_in)
+main_menu.add_command(label="Sair", command=sair)
+#main window
+canvas = Canvas(window, width=400, height=200, relief="raised", bd=1)
+canvas.place(x=200, y=150)
+#IMAGEM
+label = Label(window, text="Gestor de\nfilmes e séries", fg="red", font=("Helvitica, 20"))
+label.place(x=650, y=200)
 
-window.configure(menu=sidebar)
+window.configure(menu=main_menu)
 
 window.mainloop()
