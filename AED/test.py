6ef0b8bf2ef1d97b950b0a476e47f1924681455b
motadaniela <40210288@esmad.ipp.pt>
2022-01-22 16:09:27 +0000

    listbox comentarios

--- AED/test.py
+++ AED/test.py
@@ -1,8 +1,23 @@
-userdata = open("userdata.txt")
-line=userdata.readline()
-pos=line.index(";")
-username = line[(line.index(";", pos+1)+1):(line.index("\n"))-1]
+from tkinter import *
 
-print(username)
+window = Tk()
+window.geometry("500x500")
+window.title("Test")
 
-input()
+lbox_comentarios = Listbox(window,height=7, width=65, selectmode="single")
+lbox_comentarios.place(x=20, y=20)
+
+nome_selecao = "Nome do film"
+
+comentarios = open("comentarios.txt", "r")
+all_comments = comentarios.readlines()
+comentarios.close()
+for line in all_comments:
+    campo = line.split(";")
+    if nome_selecao not in campo:
+        lbox_comentarios.insert(END,"Ainda não existem comentários!")
+    elif campo[0] == nome_selecao:
+        for i in range(len(campo)-1,0,-1):
+            lbox_comentarios.insert(END,campo[i])
+
+window.mainloop()
